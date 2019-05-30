9eee525cb273e9626929bc4c395cb21adc223dac
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-05-30 23:26:54 +0200

    Layout actually layouting

--- public/index.php
+++ public/index.php
@@ -7,5 +7,4 @@ use League\Plates\Engine as Plates;
 require '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $templates = new Plates('..' . DIRECTORY_SEPARATOR . 'templates');
-echo $templates->render('index', ['name' => 'Jonathan']);
-
+echo $templates->render('index', []);
