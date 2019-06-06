59f339044bbad94d6e14b79c0349f9383ad212e4
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-06-06 13:30:55 +0200

    Reduced page width

--- templates/base.php
+++ templates/base.php
@@ -1,4 +1,6 @@
-<!doctype html>
+<?php
+/** @var string $title */
+?><!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -13,7 +15,9 @@
 </head>
 <body>
 <?= $this->fetch('navbar') ?>
+<div class="container">
 <?= $this->section('content') ?>
+</div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
--- templates/navbar.php
+++ templates/navbar.php
@@ -1,25 +1,27 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-    <a class="navbar-brand" href="/">Crauto</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="/personal.php">Personal</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/mfa.php">MFA</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/sessions.php">Sessions</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/accounts.php">Accounts</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/logout.php">Logout</a>
-            </li>
-        </ul>
-    </div>
+	<div class="container">
+	    <a class="navbar-brand" href="/">Crauto</a>
+	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" id="navbarNav">
+	        <ul class="navbar-nav">
+	            <li class="nav-item">
+	                <a class="nav-link" href="/personal.php">Personal</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link" href="/mfa.php">MFA</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link" href="/sessions.php">Sessions</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link" href="/accounts.php">Accounts</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link" href="/logout.php">Logout</a>
+	            </li>
+	        </ul>
+	    </div>
+	</div>
 </nav>
