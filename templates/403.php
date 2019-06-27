425c761c95f7981c3e2c3c0faa8d2f7981cdd58b
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-06-28 00:03:17 +0200

    Allow error message in 403 template
    Maybe should add a 401

--- templates/403.php
+++ templates/403.php
@@ -1,7 +1,14 @@
 <?php
 /** @var $uid string */
 /** @var $name string */
+/** @var $error ?string */
 $this->layout('base', ['title' => 'Forbidden']) ?>
 
 <h1>403 - Forbidden</h1>
 <p>You are not authorized to access this page</p>
+<?php if(isset($error) && $error !== null): ?>
+	<div class="alert alert-danger" role="alert">
+		<?= $this->e($error) ?>
+	</div>
+<?php endif ?>
+
