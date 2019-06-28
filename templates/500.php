464cedfa0ac2cb4959985fb54738cd4f3a1a53d0
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-06-29 01:45:37 +0200

    Oops

--- templates/500.php
+++ templates/500.php
@@ -2,7 +2,7 @@
 /** @var $uid string */
 /** @var $name string */
 /** @var $error ?string */
-http_response_code(505);
+http_response_code(500);
 $this->layout('base', ['title' => 'Internal Server Error']) ?>
 
 <h1>500 - Internal Server Error</h1>
