50d15c2dac82d9ea3605bdf0dd837b7827095657
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-10-06 10:31:20 +0200

    Password length as a constant

--- templates/registerform.php
+++ templates/registerform.php
@@ -34,16 +34,16 @@ $this->layout('base', ['title' => 'Register']);
 	<div class="form-group row">
 		<label for="auth-password1" class="col-sm-2 col-form-label">Password</label>
 		<div class="col-md-10">
-			<input type="password" class="form-control" id="auth-password1" name="password1" aria-describedby="password1-help" minlength="16" required>
+			<input type="password" class="form-control" id="auth-password1" name="password1" aria-describedby="password1-help" minlength="<?= CRAUTO_PASS_MIN_LENGTH ?>" required>
 			<small id="password1-help" class="form-text text-muted">
-				Your password must be <strong>at least 16 characters long</strong>. Choosing a passphrase is a <a href="https://xkcd.com/936/">good idea</a>, as it is using a password manager and a random password.
+				Your password must be <strong>at least <?= CRAUTO_PASS_MIN_LENGTH ?> characters long</strong>. Choosing a passphrase is a <a href="https://xkcd.com/936/">good idea</a>, as it is using a password manager and a random password.
 			</small>
 		</div>
 	</div>
 	<div class="form-group row">
 		<label for="auth-password2" class="col-sm-2 col-form-label">Confirm password</label>
 		<div class="col-md-10">
-			<input type="password" class="form-control" id="auth-password2" name="password2" aria-describedby="password2-help" minlength="16" required>
+			<input type="password" class="form-control" id="auth-password2" name="password2" aria-describedby="password2-help" minlength="<?= CRAUTO_PASS_MIN_LENGTH ?>" required>
 			<small id="password1-help" class="form-text text-muted">
 				Repeat the same password as above.
 			</small>
