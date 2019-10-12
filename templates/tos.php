6ed538b722127081d35aa7c9fedac457e3fbe522
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-10-12 16:45:28 +0200

    Become a lawyer

--- templates/tos.php
+++ templates/tos.php
@@ -1,8 +1,42 @@
 <?php
-$this->layout('base', ['title' => 'Terms of Service']) ?>
+$this->layout('base', ['title' => 'Privacy policy']) ?>
 
-<h1>Terms of service</h1>
-<p>TBD</p>
-
-<h1>Privacy policy</h1>
-<p>Your data is stored in a nice and cozy LDAP server. You can download it at any time from your personal page. We won't sell it or give it to third parties, except Polictecnico which isn't exactly a third party.</p>
+<h1>Informazioni sul trattamento dei dati personali (privacy policy)</h1>
+<p>In questa pagina si descrivono le modalità e le logiche del trattamento dei dati personali degli utenti (di seguito, gli "utenti") che interagendo con il Team WEEE Open (di seguito il "team") e in particolare, ma non esclusivamente, tramite questo sito (di seguito il "sito" o il "pannello di gestione account") forniscono i propri dati personali, ai sensi dell'articolo 13 del Regolamento (UE) 2016/679.</p>
+<h2>Titolare del trattamento</h2>
+<p>Titolare del trattamento è il Team WEEE Open, associazione non riconosciuta ai sensi degli articoli del 36, 37 e 38 del Codice civile. I contatti del Team WEEE Open sono disponibili sulla pagina "<a href="http://weeeopen.polito.it/contatti.html">contatti</a>" del sito web weeeopen.polito.it oppure all'indirizzo di posta elettronica composto dalla stringa <i>weeeopen</i> seguita dal simbolo @ seguito dal dominio <i>polito.it</i>.</p>
+<h2>Tipi di dati trattati</h2>
+<p>Sono trattati i dati comunicati dall'utente tramite la compilazione e l'invio, esplicito e volontario, dei moduli presenti sul sito, in aggiunta all'indirizzo email istituzionale.</p>
+<p>L'indirizzo email istituzionale degli utenti viene ottenuto concatenando la matricola, inserita dagli utenti nei moduli, alla stringa "@studenti.polito.it" o "@polito.it" a seconda del tipo di matricola immesso nei moduli.</p>
+<h2>Finalità e base giuridica del trattamento</h2>
+<p>I dati personali verranno trattati per le seguenti finalità:</p>
+<ul>
+	<li>Identificare la persona in quanto membro del team, fornire accesso alle varie applicazioni utilizzabili dai membri del team</li>
+	<li>Conservare informazioni (log) su alcune azioni compiute in alcune applicazioni consultabili dai membri del team con il fine di mantenere uno storico dei lavori effettuati. Ad esempio possono essere conservati l'username, la data, l'ora e l'azione effettuata su qualche oggetto all'interno del software di inventario, per ogni azione.</li>
+	<li>Precompilare un documento (SIR) che verrà fornito al Politecnico di Torino, per tutto il personale che accederà al laboratorio del team</li>
+	<li>Fornire qualunque altro dato al Politecnico di Torino qualora richiesto da tale ente, ad esempio per fini assicurativi, di identificazione dei membri del team, etc...</li>
+</ul>
+<p>La base giuridica del predetto trattamento è il consenso o, quando previsto, l'adempimento di obblighi legali.</p>
+<p>La comunicazione dei dati personali è necessaria per l'accesso alle applicazioni e l'accesso al laboratorio. In caso di comunicazione mancata o incompleta o inesatta dei dati potrà essere interdetto l'accesso al laboratorio o alle applicazioni.</p>
+<h2>Destinatari dei dati personali</h2>
+<p>Il destinatario dei dati raccolti è il Team WEEE Open e inoltre il Politecnico di Torino nei casi elencati precedentemente.</p>
+<h2>Modalità di trattamento e periodo di conservazione dei dati</h2>
+<p>La generazione di log, il controllo di accesso alle applicazioni e all'interno delle applicazioni, e la generazione della SIR vengono svolti in maniera automatizzata.</p>
+<p>Ogni altra operazione viene svolta in forma manuale da persone autorizzate al trattamento.</p>
+<p>I dati verranno conservati in Paesi membri dell'Unione Europea o in Svizzera, a seconda del server utilizzato al momento. La Svizzera è un Paese che la Commissione Europea ha ritenuto adeguato per il trasferimento di dati personali.</p>
+<p>I dati verranno cancellati dopo un massimo di 365 giorni dall'uscita della persona dal team (volontaria o per espulsione o in qualunque altra forma), ad eccezione dei dati presenti nei log, che potranno essere conservati più a lungo o pseudonimizzati.</p>
+<p>I dati potranno essere cancellati in qualsiasi momento dal Team WEEE Open qualora non venissero più ritenuti utili al perseguimento delle finalità indicate in precedenza.</p>
+<p>È possibile richiedere la cancellazione dei dati contattando il Team WEEE Open.</p>
+<h2>Diritti degli interessati</h2>
+<h3>Diritto di accesso ai dati e portabilità dei dati</h3>
+<p>È possibile scaricare tutti i dati immessi sul pannello di gestione account tramite lo stesso, accedendo con le credenziali scelte durante la registrazione.</p>
+<p>I dati vengono forniti in formato JSON.</p>
+<p>I dati aggiuntivi immessi in altri applicazioni possono essere scaricati tramite l'applicazione stessa, nel formato in cui sono stati immessi, o contattando il Team WEEE Open e richiedendo i dati.</p>
+<h3>Diritto di revoca del consenso al trattamento dei dati personali o cancellazione dei dati</h3>
+<p>È possibile richiedere la cancellazione di tutti i dati contattando il Team WEEE Open. I log inseriti in altre applicazioni non verranno cancellati, potranno al massimo essere pseudonimizzati se richiesto.</p>
+<p>La richiesta di eliminazione dei dati da parte degli utenti implica una rinuncia all'accesso alle applicazioni e al laboratorio e una revoca del consenso prestato al trattamento dei dati personali, senza pregiudicare la liceità del trattamento basata sul consenso prestato prima della revoca.</p>
+<h3>Diritto di rettifica dei dati</h3>
+<p>È possibile modificare parte dei dati inseriti tramite il pannello di gestione account.</p>
+<p>Per modificare i dati non modificabili dagli utenti, è necessario contattare il Team WEEE Open e fare richiesta.</p>
+<h3>Diritto di proporre reclamo a un'autorità di controllo</h3>
+<p>Gli utenti hanno diritto di proporre reclamo a un'autorità di controllo.</p>
